e2a21da96657e273364fe833accfce68a4cb2cc1
wilhsu <hsuw073@newschool.edu>
2022-02-03 21:19:21 -0500

    Initial commit

---
+++ invitation.js
@@ -0,0 +1,33 @@
+// function hello(name){ 
+//     if(name){ 
+//         return "Hello, " + name + "!"; 
+//     }else{ 
+//         return "Hello there!"; 
+//     } 
+// } 
+
+// var name = window.prompt("Enter your name: ");
+// alert("Your name is " + name);
+
+
+let a = "";
+a = window.prompt("Enter your name:");
+document.getElementById("name").innerHTML = a;
+
+function randomlinks(){
+	var myrandom=Math.round(Math.random()*2)
+	var links=new Array()
+	links[0]="https://docs.google.com/document/d/19TIYkHFn4CcI0odmcIelIVa1YrpVhGNeioJ8HMQDNLE/edit?usp=sharing"
+	links[1]="https://docs.google.com/document/d/1aa340_RNU04udoaigxgPRI2DkVlNSonCa57dQnaSuBg/edit?usp=sharing"
+	links[2]="https://docs.google.com/document/d/1TO9zhAaObEW7yc1uNrCLvMBame6iOalXilDDenPyF2E/edit?usp=sharing"
+
+	window.location=links[myrandom]
+}
+
+
+// var person = prompt("Please enter your name", "Harry Potter");
+
+// if (person != null) {
+//   document.getElementById("demo").innerHTML =
+//   "Hello " + person + "! How are you today?";
+// }
